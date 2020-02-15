79c3388c7c9f229ac64e06bafbf30a667706c544
Saif <mujawarmsaif@gmail.com>
2020-02-15 20:05:42 +0530

    Last Commit

---
+++ src/Components/Player/Player.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import classes from './Player.module.css';
+
+function Player(props) {
+	return (
+		<div className={classes.playerContainer}>
+			<audio ref={props.audioRef} onTimeUpdate={props.songProgress} onEnded={props.songEnd}>
+				<source src={props.currentSongTrack.file} />
+			</audio>
+
+			<img src={props.currentSongTrack.albumCover} alt="" />
+			<div className={classes.progressBarWrapper} onClick={props.seekClick}>
+				<div className={classes.progressBar} style={{ width: `${props.trackProgress}%` }} />
+			</div>
+			<div className={classes.Icons}>
+				<i
+					className="fas fa-random"
+					onClick={props.shuffleSong}
+					style={{ color: props.shuffleBtnStyle }}
+					title="shuffle"
+				/>
+				<i className="fas fa-step-backward" onClick={props.restartSong} title="restart" />
+				{props.SongPlayStatus ? (
+					<i className="far fa-pause-circle" onClick={props.pauseSong} title="pause" />
+				) : (
+					<i className="far fa-play-circle pBtn" onClick={props.playSong} title="play" />
+				)}
+				<i className="fas fa-step-forward" onClick={props.nextSong} title="next" />
+				<i
+					className="fas fa-undo"
+					onClick={props.repeatSong}
+					style={{ color: props.repeatBtnStyle }}
+					title="repeat"
+				/>
+			</div>
+			<div className={classes.Info}>
+				<h3>{props.currentSongTrack.track}</h3>
+				<h4>{props.currentSongTrack.artist}</h4>
+			</div>
+		</div>
+	);
+}
+
+export default Player;
